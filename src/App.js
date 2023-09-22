252fd08ffea0fa490d41078d8fc72587e698dbc6
Aravind-ibhubs <aravindyedida402@gmail.com>
2023-09-22 11:25:54 +0530

    deleting
    deletion of unwanted icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./Home/Home";
-import About from "./About/About";
-import Context from "./Context/Context";
 import Navigation from "./Navigate/Navigate";
+
+const Home = lazy(() => import("./Home/Home"));
+const About = lazy(() => import("./About/About"));
+const Context = lazy(() => import("./Context/Context"));
 
 function App() {
   return(
     <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route exact path="/" Component={Home} />
-        <Route path="/about" Component={About} />
-        <Route path="/context" Component={Context} />
-      </Routes>
+      <Suspense fallback={<div>Loading.....</div>}>
+        <Navigation />
+        <Routes>
+          <Route index Component={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/context" Component={Context} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
