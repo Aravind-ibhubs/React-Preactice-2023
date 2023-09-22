02d1e2714a08c52b2f04a56c231537c6776907b5
Aravind-ibhubs <aravindyedida402@gmail.com>
2023-09-22 11:56:44 +0530

    Added 404

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {lazy, Suspense} from "react";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Navigation from "./Navigate/Navigate";
+import NotFound from "./NotFound";
 
 const Home = lazy(() => import("./Home/Home"));
 const About = lazy(() => import("./About/About"));
 const Context = lazy(() => import("./Context/Context"));
 
 function App() {
+
   return(
     <BrowserRouter>
       <Suspense fallback={<div>Loading.....</div>}>
         <Navigation />
         <Routes>
-          <Route index Component={Home} />
+          <Route path='*' element={<NotFound />}/>
+          <Route path="/" index Component={Home} />
           <Route path="/about" Component={About} />
           <Route path="/context" Component={Context} />
         </Routes>
